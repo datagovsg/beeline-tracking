1702fdaa755b8119cb8e6994c96a2914ebabf6a4
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-01-25 17:34:34 +0800

    Add `PUT /trips/{tripId}/roster`
    Forward the request to beeline-server. If it gets through, the driver must
    be legit, so save his roster down

---
+++ src/roster.js
@@ -0,0 +1,53 @@
+const AWS = require("aws-sdk")
+const axios = require("axios")
+const jwt = require("jsonwebtoken")
+const { callbackWithFactory } = require("./callback-helpers")
+
+const makePUT = dynamoDb => (event, context, callback) => {
+  const callbackWith = callbackWithFactory(callback)
+  const { headers, pathParameters, body } = event
+  const data = JSON.parse(body)
+  const tripId = Number(pathParameters.tripId)
+  const Authorization = headers.authorization || headers.Authorization
+  axios
+    .put(`${process.env.API_URL}/trips/${tripId}/setDriver`, data, {
+      headers: { Authorization },
+    })
+    .then(response => {
+      const [, token] = (Authorization || "").split(" ")
+      const credentials = jwt.verify(token, process.env.AUTH0_SECRET)
+      const { driverId } = credentials
+      const time = Date.now()
+      const params = {
+        TableName: process.env.ROSTER_TABLE,
+        Item: { tripId, driverId, time },
+      }
+      if (data && data.vehicleId) {
+        params.Item.vehicleId = data.vehicleId
+      }
+      dynamoDb.put(params, error => {
+        if (error) {
+          console.error(error)
+          callbackWith(error.statusCode || 500, { item: params.Item, error })
+        } else {
+          callbackWith(200, response.data)
+        }
+      })
+    })
+    .catch(error => {
+      console.error(error)
+      if (error.response) {
+        const { data, status } = error.response
+        callbackWith(status, data)
+      } else {
+        callbackWith(500, { error })
+      }
+    })
+}
+
+const makeExports = dynamoDb => ({
+  makePUT,
+  put: makePUT(dynamoDb),
+})
+
+module.exports = makeExports(new AWS.DynamoDB.DocumentClient())
---
+++ test/roster-put.spec.js
@@ -0,0 +1,137 @@
+const sinon = require('sinon')
+const jwt = require('jsonwebtoken')
+const moxios = require('moxios')
+const {expect} = require('chai')
+
+const {makePUT} = require('../src/roster')
+
+describe('handler for PUTing roster', () => {
+  const mockDynamoClient = (errorOnInsert) => ({
+    put: (params, onInsert) => onInsert(errorOnInsert),
+  })
+
+  const sign = (driverId, token = process.env.AUTH0_SECRET) =>
+    `Bearer ${jwt.sign({driverId}, token)}`
+  const tripId = 1337
+  const driverId = 42
+  const vehicleId = 50
+
+  const event = {
+    body: JSON.stringify({ vehicleId }),
+    pathParameters: { tripId },
+  }
+
+  before(() => {
+    process.env.AUTH0_SECRET = 'toomanysecrets'
+    process.env.API_URL = 'https://api.beeline.sg'
+    event.headers = { Authorization: sign(driverId) }
+  })
+
+  after(() => {
+    delete process.env.AUTH0_SECRET
+    delete process.env.API_URL
+  })
+
+  beforeEach(() => {
+    moxios.install()
+  })
+
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  it('should be OK on OK', (done) => {
+    const callback = sinon.spy()
+    const handler = makePUT(mockDynamoClient())
+
+    handler(event, undefined, callback)
+
+    moxios.wait(function () {
+      const axiosResponse = {
+        status: 200,
+        response: { message: 'OK' },
+      }
+      let request = moxios.requests.mostRecent()
+      request.respondWith(axiosResponse)
+        .then(() => {
+          expect(callback.calledOnce).to.be.true
+          const [, {statusCode, body}] = callback.firstCall.args
+          expect(statusCode).equal(axiosResponse.status)
+          const response = JSON.parse(body)
+          expect(response).deep.equal(axiosResponse.response)
+          done()
+        })
+    })
+  })
+
+  it('should error on JWT error', (done) => {
+    event.headers = { Authorization: sign(driverId, 'badtoken') }
+    const callback = sinon.spy()
+    const handler = makePUT(mockDynamoClient())
+
+    handler(event, undefined, callback)
+
+    moxios.wait(function () {
+      const axiosResponse = {
+        status: 200,
+        response: { message: 'OK' },
+      }
+      let request = moxios.requests.mostRecent()
+      request.respondWith(axiosResponse)
+        .then(() => {
+          expect(callback.calledOnce).to.be.true
+          const [, {statusCode, body}] = callback.firstCall.args
+          expect(statusCode).equal(500)
+          const payload = JSON.parse(body)
+          expect(payload.error).exist
+          done()
+        })
+    })
+  })
+
+  it('should error on server error', (done) => {
+    const callback = sinon.spy()
+    const handler = makePUT(mockDynamoClient())
+
+    handler(event, undefined, callback)
+
+    moxios.wait(function () {
+      const axiosResponse = {
+        status: 403,
+        response: { message: 'fail' },
+      }
+      let request = moxios.requests.mostRecent()
+      request.respondWith(axiosResponse)
+        .then(() => {
+          expect(callback.calledOnce).to.be.true
+          const [, {statusCode, body}] = callback.firstCall.args
+          expect(statusCode).equal(axiosResponse.status)
+          expect(body).equal(JSON.stringify(axiosResponse.response))
+          done()
+        })
+    })
+  })
+
+  it('should error on server error', (done) => {
+    const callback = sinon.spy()
+    const handler = makePUT(mockDynamoClient())
+
+    handler(event, undefined, callback)
+
+    moxios.wait(function () {
+      const axiosResponse = {
+        status: 403,
+        response: { message: 'fail' },
+      }
+      let request = moxios.requests.mostRecent()
+      request.respondWith(axiosResponse)
+        .then(() => {
+          expect(callback.calledOnce).to.be.true
+          const [, {statusCode, body}] = callback.firstCall.args
+          expect(statusCode).equal(axiosResponse.status)
+          expect(body).equal(JSON.stringify(axiosResponse.response))
+          done()
+        })
+    })
+  })
+})
