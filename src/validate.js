2ff933ef9541622cfefda1fb50626eda7b8b6f9a
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-01-30 11:58:31 +0800

    Query the roster table to validate pings
    Reject the ping if there is a roster, and:
       * `driverId` does not match, or;
       * `vehicleId` is rostered and does not match

--- src/validate.js
+++ src/validate.js
@@ -2,6 +2,48 @@ const jwt = require("jsonwebtoken")
 
 const VALIDATION_ERROR = { validationError: "Ping invalid: no driver id found" }
 
+const whereTripIdIs = tripId => ({
+  ExpressionAttributeValues: {
+    ":v1": tripId,
+  },
+  KeyConditionExpression: "tripId = :v1",
+  TableName: process.env.ROSTER_TABLE,
+  ScanIndexForward: false,
+  Limit: 1,
+})
+
+const invalidDriverOrVehicle = (tripId, driverId, vehicleId) => ({
+  validationError: `Ping invalid: Trip ${tripId} should not have driver ${
+    driverId
+  } or vehicle ${vehicleId}`,
+})
+
+const validateDriverWithRoster = (driverId, event, { vehicleId }, dynamoDb) =>
+  new Promise((resolve, reject) => {
+    const tripId = Number(event.pathParameters.tripId)
+
+    dynamoDb.query(whereTripIdIs(tripId), (validationError, data) => {
+      if (validationError) {
+        console.error(validationError)
+        reject({ validationError })
+      } else {
+        const [roster] = data.Items || []
+        if (!roster) {
+          resolve({ driverId })
+        } else {
+          const valid =
+            driverId === roster.driverId &&
+            (!roster.vehicleId || vehicleId === roster.vehicleId)
+          if (!valid) {
+            reject(invalidDriverOrVehicle(tripId, driverId, vehicleId))
+          } else {
+            resolve({ driverId })
+          }
+        }
+      }
+    })
+  })
+
 /**
  * Given an AWS Lambda event and a data object containing driver and trip
  * information, verify that the driver is meant to be driving the specified
@@ -11,11 +53,13 @@ const VALIDATION_ERROR = { validationError: "Ping invalid: no driver id found" }
  *   headers contains an authorization token from a driver
  * @param {object} data
  *   an object containing a vehicleId
+ * @param {object} dynamoDb
+ *   the DynamoDB client
  * @return {Promise}
  *   resolves to driverId if the event is considered valid (see above),
  *   or rejects with validationError otherwise
  */
-function validatePing(event, data) {
+function validatePing(event, data, dynamoDb) {
   return new Promise((resolve, reject) => {
     const authorization =
       event.headers.authorization || event.headers.Authorization
@@ -29,10 +73,8 @@ function validatePing(event, data) {
       const credentials = jwt.verify(token, process.env.AUTH0_SECRET)
       const { driverId } = credentials
 
-      // TODO: look up the trip id on DynamoDB, ensure that
-      // this driver is meant to be driving this vehicle on this trip
       if (driverId) {
-        resolve(credentials)
+        resolve(validateDriverWithRoster(driverId, event, data, dynamoDb))
       } else {
         reject(VALIDATION_ERROR)
       }
--- test/validate.spec.js
+++ test/validate.spec.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken')
 
 const {validatePing} = require('../src/validate')
 
+const fail = (e) => expect.fail(undefined, undefined, JSON.stringify(e))
+
 describe('Ping validations', () => {
   const sign = (driverId, token = process.env.AUTH0_SECRET) =>
     `Bearer ${jwt.sign({driverId}, token)}`
@@ -17,8 +19,8 @@ describe('Ping validations', () => {
   })
 
   it('should reject if no token', (done) => {
-    validatePing({headers: {}})
-      .then(expect.fail)
+    validatePing({headers: {}}, {}, undefined)
+      .then(fail)
       .catch(({validationError}) => {
         expect(validationError).exist
         done()
@@ -27,8 +29,8 @@ describe('Ping validations', () => {
 
   it('should reject if bad token', (done) => {
     const authorization = sign(1, 'bad-token')
-    validatePing({headers: {authorization}})
-      .then(expect.fail)
+    validatePing({headers: {authorization}}, {}, undefined)
+      .then(fail)
       .catch(({validationError}) => {
         expect(validationError).exist
         done()
@@ -37,22 +39,130 @@ describe('Ping validations', () => {
 
   it('should reject if token has no driverId', (done) => {
     const authorization = sign(undefined)
-    validatePing({headers: {authorization}})
-      .then(expect.fail)
+    validatePing({headers: {authorization}}, {}, undefined)
+      .then(fail)
       .catch(({validationError}) => {
         expect(validationError).exist
         done()
       })
   })
 
-  it('should accept if good token', (done) => {
+  it('should reject if good token and dynamoDb fail', (done) => {
     const expected = 1337
+    const tripId = 13
+    const dynamoDb = {
+      query: (ignored, onData) => onData('fail', { Items: [] }),
+    }
     const authorization = sign(expected)
-    validatePing({headers: {authorization}})
+    const event = {
+      headers: { authorization },
+      pathParameters: { tripId },
+    }
+    validatePing(event, {}, dynamoDb)
+      .then(fail)
+      .catch(({validationError}) => {
+        expect(validationError).exist
+        done()
+      })
+  })
+
+  it('should reject if good token and non-matching roster (no vehicle)', (done) => {
+    const expected = 1337
+    const tripId = 13
+    const dynamoDb = {
+      query: (ignored, onData) => onData(undefined, { Items: [{ driverId: 69 }] }),
+    }
+    const authorization = sign(expected)
+    const event = {
+      headers: { authorization },
+      pathParameters: { tripId },
+    }
+    validatePing(event, {}, dynamoDb)
+      .then(fail)
+      .catch(({validationError}) => {
+        expect(validationError).exist
+        done()
+      })
+  })
+
+  it('should reject if good token and non-matching roster (with vehicle)', (done) => {
+    const expected = 1337
+    const tripId = 13
+    const vehicleId = 2
+    const dynamoDb = {
+      query: (ignored, onData) => onData(undefined, { Items: [{ driverId: expected, vehicleId }] }),
+    }
+    const authorization = sign(expected)
+    const event = {
+      headers: { authorization },
+      pathParameters: { tripId },
+    }
+    validatePing(event, {}, dynamoDb)
+      .then(fail)
+      .catch(({validationError}) => {
+        expect(validationError).exist
+        done()
+      })
+  })
+
+  it('should accept if good token and no roster', (done) => {
+    const expected = 1337
+    const tripId = 13
+    const dynamoDb = {
+      query: (ignored, onData) => onData(undefined, { Items: [] }),
+    }
+    const authorization = sign(expected)
+    const event = {
+      headers: { authorization },
+      pathParameters: { tripId },
+    }
+    validatePing(event, {}, dynamoDb)
       .then(({driverId}) => {
         expect(driverId).equal(expected)
         done()
       })
-      .catch(expect.fail)
+      .catch(fail)
+      .catch(done)
+  })
+
+  it('should accept if good token and valid roster (no vehicle)', (done) => {
+    const expected = 1337
+    const tripId = 13
+    const dynamoDb = {
+      query: (ignored, onData) => onData(undefined, { Items: [{ driverId: expected }] }),
+    }
+    const authorization = sign(expected)
+    const event = {
+      headers: { authorization },
+      pathParameters: { tripId },
+    }
+    validatePing(event, {}, dynamoDb)
+      .then(({driverId}) => {
+        expect(driverId).equal(expected)
+        done()
+      })
+      .catch(fail)
+      .catch(done)
+  })
+
+  it('should accept if good token and valid roster (with vehicle)', (done) => {
+    const expected = 1337
+    const tripId = 13
+    const vehicleId = 2
+    const dynamoDb = {
+      query: (ignored, onData) => onData(undefined, { Items: [{ driverId: expected, vehicleId }] }),
+    }
+    const authorization = sign(expected)
+    const event = {
+      headers: { authorization },
+      pathParameters: { tripId },
+    }
+    validatePing(event, { vehicleId }, dynamoDb)
+      .then(({driverId}) => {
+        expect(driverId).equal(expected)
+        done()
+      })
+      .catch(fail)
+      .catch(done)
   })
 })
