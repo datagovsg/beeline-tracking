33615bc871ca48bc7ae9b6b27ab474fb23619629
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-02-06 13:24:46 +0800

    Add Telegram webhook for operator welcome message
    Prep work has been undertaken so that both staging and prod chatbots
    will `POST /telegram/hook`, which responds with the welcome msg and chat id
    
    Webhook is guessable for the following reasons:
    * Telegram’s proposal to make the webhook guessable using the API token is not
    viable given that this information will be exposed in Travis via `npx serverless deploy`
    * Even on discovery, an attacker will need to work out the chat ids to send messages to
    * Even then, the most he can do is spam the operator with the standard welcome message
    
    See https://core.telegram.org/bots/api#setwebhook for more details

---
+++ src/telegram.js
@@ -0,0 +1,28 @@
+const { callbackWithFactory } = require("./callback-helpers")
+const TelegramBot = require('node-telegram-bot-api')
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN)
+
+const welcome = (chatId) => `
+Welcome to the Beeline Server monitoring bot!
+To receive notifications from me, tell your admin
+to use your chat id to set up Telegram notifications.
+Your Telegram chat id is ${chatId}
+`
+
+const hook = (event, context, callback) => {
+  const callbackWith = callbackWithFactory(callback)
+  const { message } = JSON.parse(event.body)
+  if (message && message.chat && message.chat.id) {
+    const { id: chatId } = message.chat
+    bot
+      .sendMessage(chatId, welcome(chatId))
+      .catch(console.error)
+    callbackWith(200, {})
+  } else {
+    console.warn(`Not handling ${event.body}`)
+    callbackWith(200, {})
+  }
+}
+
+module.exports = { hook }
