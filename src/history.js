5752e931a8862a772116b640fa70a79f688a90e6
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-02-13 12:34:38 +0800

    De-lint where appropriate

--- src/history.js
+++ src/history.js
@@ -1,60 +1,59 @@
-const AWS = require('aws-sdk')
-const geohash = require('ngeohash')
+const AWS = require("aws-sdk")
+const geohash = require("ngeohash")
 
-const {callbackWithFactory} = require('./callback-helpers')
+const { callbackWithFactory } = require("./callback-helpers")
 
-const addQueryStringParameters = (params, {limit, from, to}) => {
+const addQueryStringParameters = (params, { limit, from, to }) => {
   const validNumber = x => x && Number(x) > 0
   if (validNumber(limit)) {
     params.Limit = Number(limit)
   }
   if (validNumber(from) && validNumber(to)) {
-    params.KeyConditionExpression = 'tripId = :v1 and time BETWEEN :from AND :to'
-    params.ExpressionAttributeValues[':from'] = Number(from)
-    params.ExpressionAttributeValues[':to'] = Number(to)
+    params.KeyConditionExpression =
+      "tripId = :v1 and time BETWEEN :from AND :to"
+    params.ExpressionAttributeValues[":from"] = Number(from)
+    params.ExpressionAttributeValues[":to"] = Number(to)
   } else if (validNumber(from)) {
-    params.KeyConditionExpression = 'tripId = :v1 and time >= :from'
-    params.ExpressionAttributeValues[':from'] = Number(from)
+    params.KeyConditionExpression = "tripId = :v1 and time >= :from"
+    params.ExpressionAttributeValues[":from"] = Number(from)
   } else if (validNumber(to)) {
-    params.KeyConditionExpression = 'tripId = :v1 and time <= :to'
-    params.ExpressionAttributeValues[':to'] = Number(to)
+    params.KeyConditionExpression = "tripId = :v1 and time <= :to"
+    params.ExpressionAttributeValues[":to"] = Number(to)
   }
 }
 
-const makeGET = (dynamoDb) => (event, context, callback) => {
+const makeGET = dynamoDb => (event, context, callback) => {
   const callbackWith = callbackWithFactory(callback)
 
   const tripId = Number(event.pathParameters.tripId)
   const params = {
     ExpressionAttributeValues: {
-      ':v1': tripId,
+      ":v1": tripId,
     },
-    KeyConditionExpression: 'tripId = :v1',
+    KeyConditionExpression: "tripId = :v1",
     TableName: process.env.TRACKING_TABLE,
     ScanIndexForward: false,
   }
-  addQueryStringParameters(params, (event.queryStringParameters || {}))
+  addQueryStringParameters(params, event.queryStringParameters || {})
   dynamoDb.query(params, (error, data) => {
     if (error) {
       console.error(error)
-      callbackWith(500, {error})
+      callbackWith(500, { error })
     } else {
-      const body = (data.Items || []).map(
-        p => {
-          const {latitude, longitude} = geohash.decode(p.location)
-          const coordinates = {
-            type: 'Point',
-            coordinates: [longitude, latitude],
-          }
-          return Object.assign(p, {coordinates})
+      const body = (data.Items || []).map(p => {
+        const { latitude, longitude } = geohash.decode(p.location)
+        const coordinates = {
+          type: "Point",
+          coordinates: [longitude, latitude],
         }
-      )
+        return Object.assign(p, { coordinates })
+      })
       callbackWith(200, body)
     }
   })
 }
 
-const makeExports = (dynamoDb) => ({
+const makeExports = dynamoDb => ({
   makeGET,
   get: makeGET(dynamoDb),
 })
