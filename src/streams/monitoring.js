45834f591d226f9ee0310c1f8abaf26dc5543748
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-03-08 10:37:09 +0800

    Stub handling of monitoring events
    * Publish a DynamoDB stream from the events table
    * Declare and stubplement a handler for that stream that just logs, for now
    * Update serverless to 1.26.1
    
    TODO: Update serverless to 1.27.1 when available, since we need to set concurrency limits

---
+++ src/streams/monitoring.js
@@ -0,0 +1,23 @@
+// const pgp = require("pg-promise")()
+//
+// const db = pgp(process.env.DATABASE_URL)
+//
+// let lookupEventSubscriptions = db.query(`select * from eventSubscriptions`)
+
+const isPublishNoPings = record => {
+  const { OldImage, NewImage } = record.dynamodb
+  return (
+    record.eventName === "MODIFY" &&
+    NewImage.type.S === "noPings" &&
+    NewImage.time.N - OldImage.time.N > 60 * 60 * 1000 &&
+    NewImage.activeTrip.B
+  )
+}
+
+module.exports.publish = (event, context, callback) => {
+  const recordsToPublish = event.Records.filter(
+    record => record.eventName === "INSERT" || isPublishNoPings(record)
+  )
+  console.log(JSON.stringify(recordsToPublish))
+  callback(undefined, "Done.")
+}
