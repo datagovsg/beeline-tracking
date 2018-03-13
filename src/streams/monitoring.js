ab8cbbe719c4fbe8715b21339966cea2564ab11e
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-03-13 14:54:19 +0800

    Add a null guard

--- src/streams/monitoring.js
+++ src/streams/monitoring.js
@@ -58,13 +58,14 @@ const isPublishNoPings = record => {
     record.eventName === "MODIFY" &&
     NewImage.type.S === "noPings" &&
     Number(NewImage.time.N) - Number(OldImage.time.N) > 60 * 60 * 1000 &&
+    NewImage.activeTrip &&
     NewImage.activeTrip.BOOL
   if (NewImage) {
     const routeId = NewImage.trip.M.routeId.N
     console.log(
       `${routeId} - ${record.eventName} ${NewImage.type.S}
       at: ${NewImage.time.N} from: ${OldImage.time.N}
-      active: ${NewImage.activeTrip.BOOL} -> ${publish}`
+      active: ${NewImage.activeTrip && NewImage.activeTrip.BOOL} -> ${publish}`
     )
   }
   return publish
