14a3f7addc379ed176f987f862fb5061fb5cebfd
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-05-28 09:01:15 +0800

    POST /latest - log the payload to verify lambda success

--- src/latest.js
+++ src/latest.js
@@ -28,6 +28,7 @@ const makePOST = dynamoDb => (event, context, callback) => {
           console.error(error)
           callbackWith(error.statusCode || 501, { item: params.Item, error })
         } else {
+          console.log(params.Item)
           callbackWith(200, { item: params.Item })
         }
       })
